e5c58ffb278d2e64dbee518fe0821d19301de2ee
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-06-22 21:21:25 -0400

    parse search

--- scripts/parse_search.js
+++ scripts/parse_search.js
@@ -1,4 +1,7 @@
 function search_function()
 {
-    
+    var search_bar_value = document.getElementById("search_input").value;
+    const search_bar_value_arr = search_bar_value.split(" ")
+    const search_value = search_bar_value_arr.join("+")
+    console.log(search_value)
 }
