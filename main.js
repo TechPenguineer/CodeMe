a672afcf8f7f89fdbd8a0c9d69f0ddd33c89e87d
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-06-22 21:04:14 -0400

    created main styling

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function createWindow () {
     width: 800,
     height: 600,
     autoHideMenuBar: true,
+    title: "CodeMe",
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
