23ce83a40b845d5fd584cde74ce17725783c921d
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-06-22 21:29:55 -0400

    results for conf message

--- scripts/parse_search.js
+++ scripts/parse_search.js
@@ -4,4 +4,7 @@ function search_function()
     const search_bar_value_arr = search_bar_value.split(" ")
     const search_value = search_bar_value_arr.join("+")
     console.log(search_value)
+
+    const conf_res = document.getElementById("result_for_conf")
+    conf_res.textContent = `Results for "${search_bar_value}"`
 }
